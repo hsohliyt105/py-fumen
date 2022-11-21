00a8ce97e932ac362b9e025d3d78ef00f5b748f5
hsohliyt105 <62872159+hsohliyt105@users.noreply.github.com>
2022-11-22 01:51:25 +0800

    0.0.13
    fixed __init__.py

--- src/py_fumen/__init__.py
+++ src/py_fumen/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from .encoder import encode
-from .decoder import decode
-from .page import Page
-from .field import InnerField, create_inner_field, create_new_inner_field
-from .quiz import Quiz
-from .action import Action
+from .encoder import *
+from .decoder import *
+from .page import *
+from .field import *
+from .quiz import *
+from .action import *
+from .defines import *
