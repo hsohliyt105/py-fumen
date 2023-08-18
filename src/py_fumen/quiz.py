af60a374a288117b872eab164afc438aefb6f382
hsohliyt105 <62872159+hsohliyt105@users.noreply.github.com>
2023-08-19 02:26:19 +0800

    Update quiz.py
    0.1.5

--- src/py_fumen/quiz.py
+++ src/py_fumen/quiz.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
-from enum import IntEnum
+from enum import Enum
 from typing import List, Optional
 from re import search
 
 from .defines import parse_piece, parse_piece_name, Piece
 
-class Operation(IntEnum):
+class Operation(Enum):
     DIRECT = 'direct'
     SWAP = 'swap'
     STOCK = 'stock'
